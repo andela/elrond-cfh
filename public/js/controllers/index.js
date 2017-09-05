0ef408d1d3876d213e7e9b6224e7e8c6db6623a0
THEozmic <michael.ozoemena@andela.com>
2017-09-05 12:19:28 +0100

    [Feature #150681376] Add modal to home page

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,24 +1,30 @@
 angular.module('mean.system')
-.controller('IndexController', ['$scope', 'Global', '$location', 'socket', 'game', 'AvatarService', function ($scope, Global, $location, socket, game, AvatarService) {
+  .controller('IndexController', ['$scope', 'Global', '$location', 'socket', 'game', 'AvatarService', function ($scope, Global, $location, socket, game, AvatarService) {
     $scope.global = Global;
 
-    $scope.playAsGuest = function() {
+    $scope.playAsGuest = function () {
       game.joinGame();
       $location.path('/app');
     };
 
-    $scope.showError = function() {
+    $scope.showError = function () {
       if ($location.search().error) {
         return $location.search().error;
-      } else {
-        return false;
       }
+      return false;
     };
+
+    $scope.requestGameLocale = function () {
+      $.sweetModal({
+        title: 'HTML Content',
+        content: '<div><select><option>Africa</option><option>Europe</option></select>'
+      });
+      $('select').material_select();
+    }
 
     $scope.avatars = [];
     AvatarService.getAvatars()
-      .then(function(data) {
+      .then((data) => {
         $scope.avatars = data;
       });
-
-}]);
+  }]);
