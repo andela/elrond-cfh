a80bebf2029911bce6cd68d322a590b526b38cae
johadi10 <jim.hadi10@gmail.com>
2017-09-13 21:41:19 +0100

    [Feature 150397550] Fux bug

--- public/js/controllers/game.js
+++ public/js/controllers/game.js
@@ -6,6 +6,7 @@ angular.module('mean.system')
             $scope.winningCardPicked = false;
             $scope.showTable = false;
             $scope.$ = $;
+            $scope.showInviteButton = false;
             $scope.game = game;
             $scope.invitesSent = Users.invitesSent || [];
             $scope.pickedCards = [];
@@ -13,6 +14,7 @@ angular.module('mean.system')
             $scope.makeAWishFact = makeAWishFacts.pop();
             if (window.localStorage.email !== undefined) {
                 $scope.currentUserEmail = window.localStorage.email;
+                $scope.showInviteButton = true;
             }
             $scope.pickCard = function(card) {
                 if (!$scope.hasPickedCards) {
@@ -128,11 +130,9 @@ angular.module('mean.system')
             $scope.startGame = () => {
                 // check Player length
                 const $ = $scope.$;
-                if (game.players.length < game.playerMinLimit ||
-                    game.players.length > game.playerMaxLimit) {
+                if (game.players.length < game.playerMinLimit) {
                     $('.modal').modal();
                 } else {
-                    // $('.modal').modal('#modal2');
                     game.startGame();
                 }
             };
